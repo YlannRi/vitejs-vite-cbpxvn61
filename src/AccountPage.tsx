11fc631f57bbdfc03f689512e76bb5432272e6fc
Joshua Kaitharam Thomas <joshua.thomas964@gmail.com>
2026-02-23 21:39:53 +0000

    Working Ride posting and booking + maps

--- src/AccountPage.tsx
+++ src/AccountPage.tsx
@@ -1,4 +1,4 @@
-import React, {type CSSProperties, useEffect, useState} from 'react';
+import React, { type CSSProperties, useEffect, useState } from 'react';
 
 type QuickActionCardProps = {
   emoji: string;
